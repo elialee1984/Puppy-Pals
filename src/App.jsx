b621efa3781dbbb1702cb8045d4c754e0834939e
Elia Lee <elialee@MacBook-Air-von-Elia.fritz.box>
2023-08-29 23:18:42 +0200

    solved tab 4 CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { puppyList } from './data.js'
 import './App.css'
+import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
@@ -18,20 +19,24 @@ function App() {
 
   return (
     <div className="App">
-      {
-        puppies.map(puppy => {
-          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
-        })
-      }
       {featPupId && (
-        <div>
+        <div id="details">
           <h2>{featuredPup.name}</h2>
-          <ul>
+          <ul id="detailedList">
             <li>Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
           </ul>
         </div>
       )}
+      <hr></hr>
+      <p>
+        [Click name for more details:]
+        {
+          puppies.map(puppy => {
+            return <p className="listOfPuppies" onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </p>
     </div>
   )
 }

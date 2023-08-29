7e9bab0dada2700ac47ca6dd46df4c1bc13f700a
Elia Lee <elialee@MacBook-Air-von-Elia.fritz.box>
2023-08-29 22:54:11 +0200

    finished tab 3 Select a Featured Puppy

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,35 @@ import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // const handleClick = () => {
+  //   // some logic here
+  // };
 
   console.log("puppyList: ", puppyList);
+
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+
+  console.log(featuredPup);
 
   return (
     <div className="App">
       {
         puppies.map(puppy => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
         })
       }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }

a3dcfce010b99dd6b77ff58e4e6c16d2a21236c3
Elia Lee <elialee@Air-von-Elia.fritz.box>
2023-08-31 01:21:42 +0200

    minor addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { useState } from 'react'
 import { puppyList } from './data.js'
 import './App.css'
-import './App.css'
+
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
